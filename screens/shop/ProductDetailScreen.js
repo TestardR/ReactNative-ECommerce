9fbed4560a1e397cb5f32f41c68b59941af45843
unknown <rmtestard@gmail.com>
2020-03-15 17:52:06 +0100

    I have implemented a second screen ProductDetail and develop productItem styling to showcase productsOverview screen

---
+++ screens/shop/ProductDetailScreen.js
@@ -0,0 +1,33 @@
+import React from 'react';
+import {
+  ScrollView,
+  View,
+  Text,
+  Image,
+  Button,
+  StyleSheet
+} from 'react-native';
+import { useSelector } from 'react-redux';
+
+const ProductDetailScreen = props => {
+  const productId = props.navigation.getParam('productId');
+  const selectedProduct = useSelector(state =>
+    state.products.availableProducts.find(prod => prod.id === productId)
+  );
+
+  return (
+    <View>
+      <Text>{selectedProduct.title}</Text>
+    </View>
+  );
+};
+
+ProductDetailScreen.navigationOptions = props => {
+  return {
+    headerTitle: props.navigation.getParam('productTitle')
+  };
+};
+
+const styles = StyleSheet.create({});
+
+export default ProductDetailScreen;
